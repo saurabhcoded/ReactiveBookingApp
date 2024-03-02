019f6a38dfe5c79e99cb11f269155d03ae1cb766
saurabhcoded <saurabhcoded@gmail.com>
2024-03-02 14:44:27 +0530

    Changed Folder Structure : React in diffrent till the development ends

---
+++ api/resources/js/src/pages/Booking.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Stack, Avatar, Container, Typography } from "@mui/material";
+
+const Booking = () => {
+    return (
+        <Container sx={{ padding: "40px 10px" }}>
+            <Typography textAlign={"center"} component={'h1'} variant={'h4'}>
+                This is the Reactive Booking App
+            </Typography>
+        </Container>
+    );
+};
+
+export default Booking;
